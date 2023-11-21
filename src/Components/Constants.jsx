3377ed916518ee916d97fffdadf3a3f131058434
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2023-11-22 00:04:15 +0530

    Update Constants.jsx

--- src/Components/Constants.jsx
+++ src/Components/Constants.jsx
@@ -14,24 +14,9 @@ export const swiggy_api_URL =
 export const swiggy_menu_api_URL =
 "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7041&lng=77.1025&&submitAction=ENTER&restaurantId=";
 
-// shimmer card unit
-export const shimmer_card_unit = 20;
-
-// shimmer Menu card unit
-export const shimmer_menu_card_unit = 4;
-
-// Github - username
-export const Github_UserName = "ChetanNada";
-export const Github_Repository_Name = "Namaste-React";
 
 // Github API for User
 export const Github_API_User = "https://api.github.com/users/";
-
-// Social Media Links
-export const Linkedin_Link = "https://www.linkedin.com/in/chetannada/";
-export const Twitter_Link = "https://twitter.com/ChetanNada";
-export const Github_Link = "https://github.com/chetannada";
-export const Email_Link = "mailto:call2chetannada@gmail.com";
 
 // Github Authorization Token
 export const options = {
